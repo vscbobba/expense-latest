ee2228a6148f8ba7b4db14fd1105f20ca64bf2bb
chowdarybvsn <chowdarybvsn@gmail.com>
2024-01-30 19:28:42 +0530

    changed in statucheck

--- db.sh
+++ db.sh
@@ -4,25 +4,25 @@ source common.sh
 echo -e "\e[31m install software \e[0m"
 
 cp /root/expense-latest/mysql.repo /etc/yum.repos.d/mysql.repo &>$my_log
-status_check
+statuscheck
 
 
 dnf module disable mysql -y &>>$my_log
-status_check
+statuscheck
 
 echo -e "\e[33m install sql \e[0m"
 dnf install mysql-community-server -y &>>$my_log
-status_check
+statuscheck
 
 echo -e "\e[31m start mysql \e[0m"
 systemctl enable mysqld &>>$my_log
 systemctl start mysqld &>>$my_log
-status_check
+statuscheck
 
 
 echo -e "\e[35m secure install \e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1
-status_check
+statuscheck
 
 
 #mysql -uroot -pExpenseApp@1
